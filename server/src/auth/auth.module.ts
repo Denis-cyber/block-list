7e0899e3a0d1769162277d987553e29017d27860
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-10-25 22:57:27 +0300

    root/server: add signUp method

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
 import { CookieService } from './cookie.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, PasswordService, CookieService]
+  providers: [AuthService, PasswordService, CookieService],
 })
 export class AuthModule {}
