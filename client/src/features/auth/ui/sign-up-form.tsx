e49553b2c2e17513feb33da2833e65079097036c
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-11-05 00:15:34 +0300

    root/client: finish ui sign-up form

--- client/src/features/auth/ui/sign-up-form.tsx
+++ client/src/features/auth/ui/sign-up-form.tsx
@@ -1,1 +1,14 @@
-export function SignUpForm() {}
+import { UiButton } from "@/shared/ui/ui-button";
+import { UiTextField } from "@/shared/ui/ui-text-field";
+
+export function SignUpForm() {
+  return (
+    <form className="flex flex-col gap-2">
+      <UiTextField label="Email" />
+      <UiTextField label="Password" />
+      <UiButton className="mt-4" variant="primary">
+        Sign Up
+      </UiButton>
+    </form>
+  );
+}
